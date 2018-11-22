cf857e55c58657779585042a81bb7a08f91d9e06
interamaster@gmail.com <Sismed13>
2018-11-22 23:44:35 +0100

    v1 final con acceso a datos de uso para poder volver a la app anterior y exportar db a downloads

--- app/src/main/java/com/mio/jrdv/copyemails/MainActivity.java
+++ app/src/main/java/com/mio/jrdv/copyemails/MainActivity.java
@@ -614,4 +614,93 @@ public class MainActivity extends ListActivity {
 
         lastapk();
     }
+
+
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+
+    //todo importar database algun dia!!!
+
+   // https://stackoverflow.com/questions/6540906/simple-export-and-import-of-a-sqlite-database-on-android
+
+    /*
+
+
+I use this code in the SQLiteOpenHelper in one of my applications to import a database file.
+
+EDIT: I pasted my FileUtils.copyFile() method into the question.
+
+SQLiteOpenHelper
+
+
+public static String DB_FILEPATH = "/data/data/{package_name}/databases/database.db";
+
+
+ * Copies the database file at the specified location over the current
+ * internal application database.
+ *
+    public boolean importDatabase(String dbPath) throws IOException {
+
+        // Close the SQLiteOpenHelper so it will commit the created empty
+        // database to internal storage.
+        close();
+        File newDb = new File(dbPath);
+        File oldDb = new File(DB_FILEPATH);
+        if (newDb.exists()) {
+            FileUtils.copyFile(new FileInputStream(newDb), new FileOutputStream(oldDb));
+            // Access the copied database so SQLiteHelper will cache it and mark
+            // it as created.
+            getWritableDatabase().close();
+            return true;
+        }
+        return false;
+    }
+
+
+//y
+
+
+public class FileUtils {
+
+     * Creates the specified <code>toFile</code> as a byte for byte copy of the
+     * <code>fromFile</code>. If <code>toFile</code> already exists, then it
+     * will be replaced with a copy of <code>fromFile</code>. The name and path
+     * of <code>toFile</code> will be that of <code>toFile</code>.<br/>
+     * <br/>
+     * <i> Note: <code>fromFile</code> and <code>toFile</code> will be closed by
+     * this function.</i>
+     *
+     * @param fromFile
+     *            - FileInputStream for the file to copy from.
+     * @param toFile
+     *            - FileInputStream for the file to copy to.
+
+    public static void copyFile(FileInputStream fromFile, FileOutputStream toFile) throws IOException {
+        FileChannel fromChannel = null;
+        FileChannel toChannel = null;
+        try {
+            fromChannel = fromFile.getChannel();
+            toChannel = toFile.getChannel();
+            fromChannel.transferTo(0, fromChannel.size(), toChannel);
+        } finally {
+            try {
+                if (fromChannel != null) {
+                    fromChannel.close();
+                }
+            } finally {
+                if (toChannel != null) {
+                    toChannel.close();
+                }
+            }
+        }
+    }
+}
+
+//Don't forget to delete the old database file if necessary.
+
+     */
+
+
+
 }
